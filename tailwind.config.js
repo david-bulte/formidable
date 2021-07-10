43d87fb79d3b160c5179f9177b6d9c1bff63f0c2
david-bulte <david.bulte@gmail.com>
2021-07-10 18:11:13 +0200

    enable tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  // mode: process.env.TAILWIND_MODE ? 'jit' : '',
+  // purge: {
+  //   // enabled: process.env.TAILWIND_MODE === 'build',
+  //   content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}']
+  // },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
