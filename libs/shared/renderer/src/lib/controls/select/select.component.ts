a33fa84f8db06fc0c8115883d3e17ba254c007e1
david-bulte <david.bulte@gmail.com>
2021-08-22 09:31:23 +0200

    formatting

--- libs/shared/renderer/src/lib/controls/select/select.component.ts
+++ libs/shared/renderer/src/lib/controls/select/select.component.ts
@@ -3,31 +3,31 @@ import { ControlBaseComponent } from '../control-base/control-base.component';
 
 @Component({
   template: `
-    <formidable-control-base [formGroup]='parent'>
-      <ng-container [ngSwitch]='formElement.props.mode'>
+    <formidable-control-base [formGroup]="parent">
+      <ng-container [ngSwitch]="formElement.props.mode">
         <div *ngSwitchCase="'radio'">
           <label
-            class='mb-4 flex items-center'
-            *ngFor='let option of formElement.props.options'
+            class="mb-4 flex items-center"
+            *ngFor="let option of formElement.props.options"
           >
             <input
-              type='radio'
-              class='form-radio'
-              [value]='option.value'
-              [formControlName]='formControlName'
+              type="radio"
+              class="form-radio"
+              [value]="option.value"
+              [formControlName]="formControlName"
             />
-            <span class='ml-2'>{{ option.label }}</span>
+            <span class="ml-2">{{ option.label }}</span>
           </label>
         </div>
 
         <select
-          class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-          [formControlName]='formControlName'
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          [formControlName]="formControlName"
           *ngSwitchDefault
         >
           <option
-            [value]='option.value'
-            *ngFor='let option of formElement.props.options'
+            [value]="option.value"
+            *ngFor="let option of formElement.props.options"
           >
             {{ option.label }}
           </option>
